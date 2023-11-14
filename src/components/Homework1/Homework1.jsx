f286c8cea287339e79b21418c1d46e42262538d0
SSSerg05 <ssserg05@gmail.com>
2023-11-14 17:36:00 +0200

    fix task 6

--- src/components/Homework1/Homework1.jsx
+++ src/components/Homework1/Homework1.jsx
@@ -22,16 +22,28 @@ export class Homework1 extends Component {
     this.s2 += 50;
     const val = this.state.s1 + 50;
     this.setState({s1: val})
+    console.log('btn1: s2',this.s2);
+    console.log('btn1: s1',this.state.s1); // old value -> old render
   }
 
+  // test (Task 6)
+  buttonHander2 = () => {
+    this.s2 += 150;
+    console.log('btn2: s2',this.s2);      // no render -> it's not state value
+    console.log('btn2: s1',this.state.s1); 
+  }
+
+
   render() {
-    const styleBtn = {padding: '15px', };
+    const styleBtn = { padding: '15px', marginRight: '5px', };
 
     return (
       <>
         <div>State: s1={this.state.s1}</div>
         <div>s2={this.s2}</div>
         <button type='button' onClick={this.buttonHander} style={styleBtn}>Add +50</button>
+        {/* for Task 6 */}
+        <button type='button' onClick={this.buttonHander2} style={styleBtn}>Add +150</button>
       </>
     )
   };
